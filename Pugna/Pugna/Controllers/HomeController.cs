fa96d7f2c534cdae9b35098795d013223a3c4b07
BorislavNikolov <borislav_nikolov99@abv.bg>
2020-03-20 20:31:08 +0200

    Create some views and controllers

--- Pugna/Pugna/Controllers/HomeController.cs
+++ Pugna/Pugna/Controllers/HomeController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Pugna.Models;
-
-namespace Pugna.Controllers
+﻿namespace Pugna.Controllers
 {
+    using System.Diagnostics;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    using Pugna.Models;
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
