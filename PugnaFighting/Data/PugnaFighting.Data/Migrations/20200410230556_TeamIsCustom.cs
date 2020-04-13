345e9ad799e6b2cb3bf2aca44846b10bc1ef3985
BorislavNikolov <borislav_nikolov99@abv.bg>
2020-04-13 18:02:16 +0300

    Edit Account Page.

--- PugnaFighting/Data/PugnaFighting.Data/Migrations/20200410230556_TeamIsCustom.cs
+++ PugnaFighting/Data/PugnaFighting.Data/Migrations/20200410230556_TeamIsCustom.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
-
-namespace PugnaFighting.Data.Migrations
+﻿namespace PugnaFighting.Data.Migrations
 {
+    using Microsoft.EntityFrameworkCore.Migrations;
+
     public partial class TeamIsCustom : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
