a8fedf6072342e777bb47e591a11878de7d57d92
BorislavNikolov <borislav_nikolov99@abv.bg>
2020-04-14 12:01:41 +0300

    Implement JS function for confirm delete.

---
+++ PugnaFighting/Web/PugnaFighting.Web/wwwroot/js/confirmDelete.js
@@ -0,0 +1,18 @@
+﻿let sellSpan = document.getElementById('delete-span');
+let deleteQuestion = document.getElementById('delete-question');
+let deleteButton = document.getElementById('delete-btn');
+let cancelButton = document.getElementById('cancel-btn');
+
+sellSpan.addEventListener('click', () => {
+    sellSpan.style.display = 'none';
+    deleteQuestion.style.display = 'block';
+    deleteButton.style.display = 'block';
+    cancelButton.style.display = 'block';
+});
+
+cancelButton.addEventListener('click', () => {
+    sellSpan.style.display = 'block';
+    deleteQuestion.style.display = 'none';
+    deleteButton.style.display = 'none';
+    cancelButton.style.display = 'none';
+});
